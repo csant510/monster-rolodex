fc9ccc1ebe5bb22013c0c6a0cd937bcd7f135683
csant510 <james.sant83@gmail.com>
2020-03-12 19:13:25 -0700

    Updates

---
+++ precache-manifest.299af9102245415e568462bf47cab651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6855fa3dfc5dba88546b3c1aaab0b5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "999c6f012783cce58d69",
+    "url": "/monster-rolodex/static/css/main.52a3f84e.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/monster-rolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999c6f012783cce58d69",
+    "url": "/monster-rolodex/static/js/main.677d5fb4.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
